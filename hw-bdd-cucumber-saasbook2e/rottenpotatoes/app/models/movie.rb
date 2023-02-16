c7df1339ddaaaaeb677b7e857294721dac188681
Kanyana <ktoadithep@gmail.com>
2023-02-16 23:05:10 +0700

    def

--- hw-bdd-cucumber-saasbook2e/rottenpotatoes/app/models/movie.rb
+++ hw-bdd-cucumber-saasbook2e/rottenpotatoes/app/models/movie.rb
@@ -3,4 +3,11 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+  def self.with_ratings(ratings, sort_by)
+    if ratings.nil?
+      all.order sort_by
+    else
+      where(rating: ratings.map(&:upcase)).order sort_by
+    end
+  end
 end
